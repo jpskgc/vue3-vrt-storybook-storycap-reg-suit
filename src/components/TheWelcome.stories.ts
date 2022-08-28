b1cc3fb9207e7ed8106a18593dfc1a7f15d26e1c
jpskgc <jpskgc.v3@gmail.com>
2022-08-29 01:21:27 +0900

    [add] storybook名変更

--- src/components/TheWelcome.stories.ts
+++ src/components/TheWelcome.stories.ts
@@ -3,16 +3,12 @@ import  '../assets/base.css'
 import type { Meta } from '@storybook/vue3';
 
 export default {
-    /* 👇 The title prop is optional.
-     * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
-     * to learn how to generate automatic titles
-     */
     title: 'TheWelcome',
     component: TheWelcome,
     decorators: [() => ({ template: '<div style="margin: 3em;"><story /></div>' })],
 } as Meta<typeof TheWelcome>;
 
-export const Primary = () => ({
+export const Sample = () => ({
     components: { TheWelcome },
     template: '<TheWelcome> </TheWelcome>',
 });
